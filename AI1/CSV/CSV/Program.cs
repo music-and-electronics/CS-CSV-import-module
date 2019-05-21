3a42e2a65bf3f35f3559390b40187fb63777713f
musicandelectronics <kjh970407@naver.com>
2019-05-21 13:22:39 +0900

    New Project

--- AI1/CSV/CSV/Program.cs
+++ AI1/CSV/CSV/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
-
+using System.IO;
+using System.Collections.Generic;
 namespace CSV
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
-			Console.WriteLine ("It is changed!");
-			Console.WriteLine ("You droped ; ");
+			StreamReader sr= new StreamReader("/home/dextop/DataSet.csv");
+			List<string[]> file_parse = new List<string[]> ();
+			while(!sr.EndOfStream)
+			{
+				string line = sr.ReadLine ();
+				string[] data= line.Split(',');
+				file_parse.Add (data);
+			}
+
+			foreach(string[] a in file_parse)
+			{
+				for(int i=0;i<a.Length;i++)
+				{
+					Console.Write (a [i]);
+				}
+				Console.WriteLine ();
+			}
+
+			
 		}
 	}
 }
